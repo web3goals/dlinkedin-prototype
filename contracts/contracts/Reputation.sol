955b5217c8dd5ccf0f5cd0451c13263f0f0c2352
kiv1n <aloha@kiv1n.ru>
2023-11-22 16:56:18 +0300

    Add statements to reputation contract

--- contracts/contracts/Reputation.sol
+++ contracts/contracts/Reputation.sol
@@ -3,7 +3,20 @@ pragma solidity ^0.8.9;
 
 import "@lukso/lsp-smart-contracts/contracts/LSP8IdentifiableDigitalAsset/LSP8IdentifiableDigitalAsset.sol";
 
+/**
+ * @notice A contract that stores account reputations.
+ */
 contract Reputation is LSP8IdentifiableDigitalAsset {
+    struct Statement {
+        address author;
+        uint time;
+        uint skill;
+        uint evaluation;
+        string extraData;
+    }
+
+    mapping(bytes32 => Statement[]) _statements;
+
     constructor()
         LSP8IdentifiableDigitalAsset(
             "dLinkedIn - Reputations",
@@ -21,11 +34,38 @@ contract Reputation is LSP8IdentifiableDigitalAsset {
         _mint(msg.sender, bytes32(uint256(uint160(msg.sender))), true, "0x");
     }
 
+    function postStatement(
+        address account,
+        uint skill,
+        uint evaluation,
+        string memory data
+    ) external {
+        bytes32 tokenId = _accountToBytes32(account);
+        _existsOrError(tokenId);
+        _statements[tokenId].push(
+            Statement(msg.sender, block.timestamp, skill, evaluation, data)
+        );
+    }
+
     /// ***********************************
     /// ***** EXTERNAL VIEW FUNCTIONS *****
     /// ***********************************
 
+    function getStatements(
+        address account
+    ) external view returns (Statement[] memory) {
+        bytes32 tokenId = _accountToBytes32(account);
+        _existsOrError(tokenId);
+        return _statements[tokenId];
+    }
+
     /// ******************************
     /// ***** INTERNAL FUNCTIONS *****
     /// ******************************
+
+    function _accountToBytes32(
+        address account
+    ) internal pure returns (bytes32) {
+        return bytes32(uint256(uint160(account)));
+    }
 }
--- contracts/test/Reputation.ts
+++ contracts/test/Reputation.ts
@@ -5,7 +5,7 @@ import { ethers } from "hardhat";
 describe("Reputation", function () {
   async function initFixture() {
     // Get signers
-    const [deployer, userOne, userTwo] = await ethers.getSigners();
+    const [deployer, userOne, userTwo, userThree] = await ethers.getSigners();
     // Deploy reputation contract
     const reputationContractFactory = await ethers.getContractFactory(
       "Reputation"
@@ -16,19 +16,32 @@ describe("Reputation", function () {
       deployer,
       userOne,
       userTwo,
+      userThree,
       reputationContract,
     };
   }
 
   it("Should support the main flow", async function () {
-    const { userOne, userTwo, reputationContract } = await loadFixture(
-      initFixture
-    );
+    const { userOne, userTwo, userThree, reputationContract } =
+      await loadFixture(initFixture);
     // Create and check reputation
     expect(await reputationContract.balanceOf(userOne)).to.be.equal(0);
     await expect(reputationContract.connect(userOne).create()).to.be.not
       .reverted;
     await expect(reputationContract.connect(userOne).create()).to.be.reverted;
     expect(await reputationContract.balanceOf(userOne)).to.be.equal(1);
+    // Post statements
+    await expect(
+      reputationContract
+        .connect(userTwo)
+        .postStatement(userOne, 0, 5, "ipfs://statement-1")
+    ).to.be.not.reverted;
+    await expect(
+      reputationContract
+        .connect(userThree)
+        .postStatement(userOne, 1, 3, "ipfs://statement-2")
+    ).to.be.not.reverted;
+    const statements = await reputationContract.getStatements(userOne);
+    expect(statements.length).to.be.equal(2);
   });
 });
